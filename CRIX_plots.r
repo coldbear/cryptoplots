a43e0ad5347c306d8622d7eb25e4c9b3fd5404e5
coldbear <coldbear@users.noreply.github.com>
2017-12-25 11:13:28 +0100

    Update CRIX_plots.r
    bug fixed

--- CRIX_plots.r
+++ CRIX_plots.r
@@ -16,13 +16,13 @@ require(zoo)
 #getting returns
 
 returns= data.frame(date = as.yearmon(crix$date[-1], "%y-%m"), returns = diff(log(crix$price)))
-agg.month= aggregate(returns ~ MY, ret.table, sd)
-agg.days = aggregate(returns ~ MY, ret.table, length)
-agg.month$days=days$returns
-agg.month$MY=as.Date(agg.month$MY)
+agg.month= aggregate(returns ~ date, returns, sd)
+agg.days = aggregate(returns ~ date, returns, length)
+agg.month$days=agg.days$returns
+agg.month$date=as.Date(agg.month$date)
 colnames(agg.month)=c("date", "month.std", "days")
 agg.month$monthlyvola=sqrt(agg.month$days)*agg.month$month.std*100
 
 plot(agg.month$date, agg.month$monthlyvola, type = "l", col = "grey",lwd = 3, xlab = "Date", 
-ylab = "Monthly aggregated returns volatility")
+     ylab = "Monthly aggregated returns volatility")
 axis(1,at=agg.month$date,labels=format(agg.month$date,"%Y-%m"),las=1)
